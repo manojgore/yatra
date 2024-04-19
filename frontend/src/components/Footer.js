1a17652fa7ef77944a429519fcf606ce51cad3f7
MANOJ GORE <manojgore.mg@gmail.com>
2024-04-19 16:58:42 +0530

    footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,29 +3,29 @@ import React from 'react'
 function Footer() {
     return (
         <>
-            <!-- news letter section -->
+            {/* <!-- news letter section --> */}
             <section class="news-letter-section pb-0 pt-0">
                 <div class="boxcar-container">
                     <div class="row g-0 cus-radius-16">
-                        <!-- image-column -->
+                        {/* <!-- image-column --> */}
                         <div class="image-column col-lg-4 col-md-6 col-sm-12">
                             <div class="inner-column">
                                 <div class="image-box">
-                                    <figure class="image"><img src="images/resource/news1-1.png" alt=""></figure>
+                                    <figure class="image"><img src="images/resource/news1-1.png" alt="" /></figure>
                                 </div>
                             </div>
                         </div>
-                        <!-- content-column -->
+                        {/* <!-- content-column --> */}
                         <div class="form-column col-lg-8 col-md-12 col-sm-12">
                             <div class="inner-column">
                                 <div class="boxcar-title light">
-                                    <h2>Subscribe To Our Mailing <br>List And Stay Up To Date</h2>
+                                    <h2>Subscribe To Our Mailing <br />List And Stay Up To Date</h2>
                                     <div class="text">We'll keep you updated with the best new jobs.</div>
                                 </div>
                                 <div class="subscribe-form wow fadeInUp" data-wow-delay="100ms">
                                     <form method="post" action="#">
                                         <div class="form-group">
-                                            <input type="email" name="email" class="email" value="" placeholder="Your email" required="">
+                                            <input type="email" name="email" class="email" value="" placeholder="Your email" required="" />
                                                 <button type="button" class="theme-btn btn-style-one hover-light"><span class="btn-title">subscribe</span></button>
                                         </div>
                                     </form>
@@ -35,14 +35,14 @@ function Footer() {
                     </div>
                 </div>
             </section>
-            <!-- End news-letter-section -->
+            {/* <!-- End news-letter-section -->
 
-            <!-- main footer -->
+            <!-- main footer --> */}
             <footer class="boxcar-footer footer-style-two v8">
                 <div class="widgets-section">
                     <div class="boxcar-container">
                         <div class="row">
-                            <!-- Footer COlumn -->
+                            {/* <!-- Footer COlumn --> */}
                             <div class="footer-column-two col-lg-9 col-md-6 col-sm-12">
                                 <div class="row">
                                     <div class="col-lg-3 col-md-6 col-sm-12">
@@ -109,7 +109,7 @@ function Footer() {
                                     </div>
                                 </div>
                             </div>
-                            <!-- footer column -->
+                            {/* <!-- footer column --> */}
                             <div class="footer-column col-lg-3 col-md-6 col-sm-12">
                                 <div class="footer-widget social-widget wow fadeInUp" data-wow-delay="400ms">
                                     <h4 class="widget-title">Vehicles Type</h4>
@@ -134,11 +134,11 @@ function Footer() {
                         </div>
                     </div>
                 </div>
-                <!--  Footer Bottom -->
+                {/* <!--  Footer Bottom --> */}
                 <div class="footer-bottom">
                     <div class="boxcar-container">
                         <div class="inner-container">
-                            <div class="copyright-text wow fadeInUp">© <a href="#">2024 Boxcars.com. All rights reserved.</div>
+                            <div class="copyright-text wow fadeInUp">© <a href="/">2024 YatraCars.com.</a> All rights reserved.</div>
 
                             <ul class="footer-nav wow fadeInUp" data-wow-delay="100ms">
                                 <li><a href="#">Terms & Conditions</a></li>
@@ -148,7 +148,7 @@ function Footer() {
                     </div>
                 </div>
             </footer>
-            <!-- End boxcar-footer -->
+            {/* <!-- End boxcar-footer --> */}
         </>
     )
 }
