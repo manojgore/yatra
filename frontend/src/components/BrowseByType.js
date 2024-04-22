0975ae79b03416329cd7e41e7e3d71b0694c05cd
MANOJ GORE <manojgore.mg@gmail.com>
2024-04-22 08:30:08 +0530

    rgf

--- frontend/src/components/BrowseByType.js
+++ frontend/src/components/BrowseByType.js
@@ -132,7 +132,7 @@ function BrowseByType() {
                         </div>
                         {/* <!-- cars-block --> */}
                         <div class="cars-block-six home-8">
-                            <div class="inner-box wow fadeInUp" data-wow-delay="300ms">
+                            <div class="inner-box wow fadeInUp" data-wow-delay="300ms"> 
                                 <div class="image-box">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34" fill="none">
                                         <mask id="mask0_130_4528" style={{maskType:"luminance"}} maskUnits="userSpaceOnUse" x="0" y="0" width="34" height="34">
