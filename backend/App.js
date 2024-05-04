67104e8791cbda0bd45979bb8e0c0a18af75aadf
MANOJ GORE <manojgore.mg@gmail.com>
2024-05-04 09:18:00 +0530

    sdf

--- backend/App.js
+++ backend/App.js
@@ -23,7 +23,7 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
   if (err) {
-    console.error('Error connecting to MySQL:', err);
+    console.error('Error connecting to MySQL:', err); 
     return;
   }
   console.log('Connected to MySQL');  
