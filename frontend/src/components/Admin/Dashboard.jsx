0b5e197baf21ddb5f5fc23a148299fc624fa3e24
MANOJ GORE <manojgore.mg@gmail.com>
2024-05-07 11:59:34 +0530

    tyg

--- frontend/src/components/Admin/Dashboard.jsx
+++ frontend/src/components/Admin/Dashboard.jsx
@@ -16,7 +16,6 @@ function Dashboard() {
                                     <div class="logo-inner">
                                         <div class="logo"><a href="index.html"><img src="images/logo.svg" alt="" title="Boxcar" /></a></div>
                                         <div class="btn-box">
-
                                             <div class="layout-search style1">
                                                 <div class="search-box">
                                                     <svg class="icon" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
