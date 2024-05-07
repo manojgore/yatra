d3b91da1c986550c6039478145fc330eb540c659
MANOJ GORE <manojgore.mg@gmail.com>
2024-05-07 12:01:27 +0530

    mnj

--- frontend/src/components/Fleet.js
+++ frontend/src/components/Fleet.js
@@ -143,7 +143,7 @@ function Fleet() {
                                     <li><a href="#">Used Kia Cars</a></li>
                                     <li><a href="#">Used Land Rover Cars</a></li>
                                     <li><a href="#">Used Lexus Cars</a></li>
-                                    <li><a href="#">Used Mazda Cars</a></li>
+                                    <li><a href="#">Used Mazda Cars</a></li> 
                                     <li><a href="#">Used Mercedes-Benz Cars</a></li>
                                     <li><a href="#">Used MG Cars</a></li>
                                 </ul>
