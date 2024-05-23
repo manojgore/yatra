10185793188a5320f3e87be9e62edac06a180160
mgore <manojgore.mg@gmail.com>
2024-05-23 21:02:27 +0530

    sdf

--- frontend/src/components/Fleet.js
+++ frontend/src/components/Fleet.js
@@ -6,7 +6,7 @@ function Fleet() {
             {/* <!-- cars-section --> */}
             <section class="cars-section">
                 <div class="boxcar-container">
-                    <div class="boxcar-title wow fadeInUp"> 
+                    <div class="boxcar-title wow fadeInUp">  
                         <h2>Fleet</h2> 
                         <a href="#" class="btn-title">View More<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewbox="0 0 14 14" fill="none"><g clip-path="url(#clip0_601_3199)"><path d="M13.6109 0H5.05533C4.84037 0 4.66643 0.173943 4.66643 0.388901C4.66643 0.603859 4.84037 0.777802 5.05533 0.777802H12.6721L0.113697 13.3362C-0.0382246 13.4881 -0.0382246 13.7342 0.113697 13.8861C0.18964 13.962 0.289171 14 0.388666 14C0.488161 14 0.587656 13.962 0.663635 13.8861L13.222 1.3277V8.94447C13.222 9.15943 13.3959 9.33337 13.6109 9.33337C13.8259 9.33337 13.9998 9.15943 13.9998 8.94447V0.388901C13.9998 0.173943 13.8258 0 13.6109 0Z" fill="#050B20"></path></g><defs><clippath id="clip0_601_3199"><rect width="14" height="14" fill="white"></rect></clippath></defs></svg></a>
                     </div>
