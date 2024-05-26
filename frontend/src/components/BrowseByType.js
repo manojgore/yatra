b9be894d6e7ea0f81a71ec9fce78a667b944d9f1
MANOJ GORE <manojgore.mg@gmail.com>
2024-05-26 20:05:19 +0530

    bnml

--- frontend/src/components/BrowseByType.js
+++ frontend/src/components/BrowseByType.js
@@ -154,7 +154,7 @@ function BrowseByType() {
                                     <h6 class="title"><a href="#">Van</a></h6> 
                                 </div>
                             </div>  
-                        </div>
+                        </div> 
                         {/* <!-- cars-block --> */}
                         <div class="cars-block-six home-8">
                             <div class="inner-box wow fadeInUp" data-wow-delay="400ms">
