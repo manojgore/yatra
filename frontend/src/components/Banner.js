4d17f2d324eae5e7080a758ace10bc0d90839e9e
MANOJ GORE <manojgore.mg@gmail.com>
2024-05-28 19:00:42 +0530

    ytrfg

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -61,7 +61,7 @@ function Banner() {
                                     </div>
                                     <div className="form-tab-pane" id="tab-2">
                                         <form>
-                                            <div className="form_boxes">
+                                            <div className="form_boxes"> 
                                                 <div className="drop-menu">
                                                     <label>Any Makes</label>
                                                     <div className="select">
