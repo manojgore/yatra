22bbb255c18775ebab0a18b35b06e945fd258de1
MANOJ GORE <manojgore.mg@gmail.com>
2024-05-30 21:43:36 +0530

    rffg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-route
 
 import logo from './logo.svg'; 
 import './App.css';
-import Navbar from './components/Navbar'; 
+import Navbar from './components/Navbar';  
 import Banner from './components/Banner';
-import Fleet from './components/Fleet';  
+import Fleet from './components/Fleet';   
 import Footer from './components/Footer'; 
 import BrowseByType from './components/BrowseByType';
 // import Dashboard from './components/Admin/Dashboard';
