54708ad5b471231bf5b78fc38808811db98be0fa
MANOJ GORE <manojgore.mg@gmail.com>
2024-06-05 20:40:53 +0530

    vcfgh

--- backend/App.js
+++ backend/App.js
@@ -11,7 +11,7 @@ const app = express();
 const port = 3000; 
 
 // Middleware to parse JSON bodies
-app.use(bodyParser.json());
+app.use(bodyParser.json()); 
 
 // MySQL database connection
 const db = mysql.createConnection({  
