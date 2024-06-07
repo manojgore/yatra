1bb910efaf43e754a5a956363cc7debbae873d66
MANOJ GORE <manojgore.mg@gmail.com>
2024-06-07 23:25:43 +0530

    mjhg

--- frontend/src/components/Fleet.js
+++ frontend/src/components/Fleet.js
@@ -137,7 +137,7 @@ function Fleet() {
                                     <li><a href="#">Used Hyundai Cars</a></li> 
                                     <li><a href="#">Used Infiniti Cars</a></li>
                                     <li><a href="#">Used Jaguar Cars</a></li> 
-                                    <li><a href="#">Used Jeep Cars</a></li>
+                                    <li><a href="#">Used Jeep Cars</a></li> 
                                 </ul>
                                 <ul class="cars-list"> 
                                     <li><a href="#">Used Kia Cars</a></li>
