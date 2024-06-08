ac7ffffae21794fb80ef39165db212b35beba1ea
MANOJ GORE <manojgore.mg@gmail.com>
2024-06-08 19:08:49 +0530

    trdf

--- backend/App.js
+++ backend/App.js
@@ -8,7 +8,7 @@ const nodemailer = require('nodemailer');
 
 // Create an Express app
 const app = express();
-const port = 3000; 
+const port = 3000;  
 
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());  
