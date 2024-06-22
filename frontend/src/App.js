ad33fc3adc8eed48e19ead958d5c9a5ab7159803
MANOJ GORE <manojgore.mg@gmail.com>
2024-06-22 08:39:45 +0530

    rtjig

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-route
 import logo from './logo.svg'; 
 import './App.css';
 import Navbar from './components/Navbar';   
-import Banner from './components/Banner'; 
+import Banner from './components/Banner';  
 import Fleet from './components/Fleet';   
 import Footer from './components/Footer';     
 import BrowseByType from './components/BrowseByType';    
@@ -14,11 +14,11 @@ function App() {
   return (    
     <> 
     {/* <AppContent /> */}
-    <Navbar />  
-    <Banner />     
-    <BrowseByType /> 
-    <Fleet /> 
-    <Footer />  
+    <Navbar />
+    <Banner />
+    <BrowseByType />
+    <Fleet />
+    <Footer />
     </>
   );
 }
