e1f6c082b21855bca5b40850ad407a95c226438a
MANOJ GORE <manojgore.mg@gmail.com>
2024-07-02 20:09:27 +0530

    oiu

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router } from "react-router-dom";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';          
+import App from './App'; 
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); 
 root.render(
