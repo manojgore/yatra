43a776904aa5d67bd96d608f5f6dea14845c0bd7
MANOJ GORE <manojgore.mg@gmail.com>
2024-07-15 09:38:35 +0530

    yhk

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -27,7 +27,7 @@ const Navbar = () => {
                                                         <img src="images/resource/car-search.jpg" alt="img" />    
                                                     </div>  
                                                     <div className="info"> 
-                                                        <p className="name">Audi, Q5 - 2023 C300e AMG Line Night Ed Premium Plus 5dr 9G-Tronic</p>
+                                                        <p className="name">Audi, Q5 - 2023 C300e AMG Line Night Ed Premium Plus 5dr 9G-Tronic</p> 
                                                         <span className="price">$399</span> 
                                                     </div>   
                                                 </a></li>    
