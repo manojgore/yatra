7ab7b7845ffb9d445cd34b32c2dba8315d1ba0e5
MANOJ GORE <manojgore.mg@gmail.com>
2024-07-26 08:49:00 +0530

    ghgh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,10 @@ import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-route
 
 import logo from './logo.svg'; 
 import './App.css'; 
-import Navbar from './components/Navbar';     
-import Banner from './components/Banner';    
-import Fleet from './components/Fleet';     
-import Footer from './components/Footer';       
+import Navbar from './components/Navbar';
+import Banner from './components/Banner';
+import Fleet from './components/Fleet';
+import Footer from './components/Footer';        
 import BrowseByType from './components/BrowseByType';     
 // import Dashboard from './components/Admin/Dashboard';
 
