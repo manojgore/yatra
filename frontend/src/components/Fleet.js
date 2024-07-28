29515a9f818a1051d801ea0531c6add341830e5f
mgore <manojgore.mg@gmail.com>
2024-07-28 09:27:03 +0530

    cvb

--- frontend/src/components/Fleet.js
+++ frontend/src/components/Fleet.js
@@ -53,7 +53,7 @@ function Fleet() {
                                 </ul>
                                 <ul class="cars-list">
                                     <li><a href="#">Used Kia Cars</a></li>
-                                    <li><a href="#">Used Land Rover Cars</a></li>
+                                    <li><a href="#">Used Land Rover Cars</a></li> 
                                     <li><a href="#">Used Lexus Cars</a></li> 
                                     <li><a href="#">Used Mazda Cars</a></li>
                                     <li><a href="#">Used Mercedes-Benz Cars</a></li>
