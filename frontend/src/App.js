93f24bc3bce6a934058dade736741b36cb794265
mgore <manojgore.mg@gmail.com>
2024-08-02 14:28:10 +0530

    ygh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
-import logo from './logo.svg'; 
-import './App.css'; 
+import logo from './logo.svg';
+import './App.css';
 import Navbar from './components/Navbar';
-import Banner from './components/Banner'; 
-import Fleet from './components/Fleet';  
-import Footer from './components/Footer';          
-import BrowseByType from './components/BrowseByType';     
+import Banner from './components/Banner';
+import Fleet from './components/Fleet';
+import Footer from './components/Footer';     
+import BrowseByType from './components/BrowseByType';
 // import Dashboard from './components/Admin/Dashboard';
 
-function App() {  
+function App() {   
   return (    
     <> 
     {/* <AppContent /> */}  
