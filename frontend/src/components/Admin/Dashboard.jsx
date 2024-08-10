4af2a90487f8d49e4e207db5599b119f0754c0c2
mgore <manojgore.mg@gmail.com>
2024-08-10 09:15:48 +0530

    xcfvb

--- frontend/src/components/Admin/Dashboard.jsx
+++ frontend/src/components/Admin/Dashboard.jsx
@@ -61,7 +61,7 @@ function Dashboard() {
                                                                 <p class="name">Audi, Q5 - 2023 C300e AMG Line Night Ed Premium Plus 5dr 9G-Tronic</p>
                                                                 <span class="price">$399</span> 
                                                             </div> 
-                                                        </a></li>
+                                                        </a></li> 
                                                     </ul> 
                                                     <a href="inventory-page-single.html" class="btn-view-search">  
                                                         View Details
