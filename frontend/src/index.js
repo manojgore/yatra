d7a3dd7fdcaf00108072e243d5a6f81061e562f9
mgore <manojgore.mg@gmail.com>
2024-08-28 08:12:58 +0530

    d

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import React from 'react';
 import ReactDOM from 'react-dom/client'; 
-import './index.css';   
+import './index.css';    
 import App from './App';     
 import reportWebVitals from './reportWebVitals';  
   
