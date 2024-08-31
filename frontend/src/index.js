f02ad586e2673d2abd3d43d3e7f3ed2f2c7fbf3f
mgore <manojgore.mg@gmail.com>
2024-08-31 09:01:24 +0530

    d

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router } from "react-router-dom";
 
 import React from 'react';
-import ReactDOM from 'react-dom/client'; 
+import ReactDOM from 'react-dom/client';  
 import './index.css';    
 import App from './App';     
 import reportWebVitals from './reportWebVitals';  
