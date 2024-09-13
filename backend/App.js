9a9c8aa6009fabc3e6544f929bdfcc47b18678d5
mgore <manojgore.mg@gmail.com>
2024-09-13 10:16:15 +0530

    e

--- backend/App.js
+++ backend/App.js
@@ -34,7 +34,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',  
   auth: {  
     user: 'your_email@gmail.com', 
-    pass: 'your_email_password'  
+    pass: 'your_email_password'   
   } 
 }); 
 
