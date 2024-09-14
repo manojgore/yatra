7367dd886391fe82f7bd9e389b252162ba51ad23
mgore <manojgore.mg@gmail.com>
2024-09-14 06:13:33 +0530

    h

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,8 @@ const express = require(`express`)
 
 const app = express() 
 const PORT = 5000 
-
-app.get("/",(req,res)=>{   
+ 
+app.get("/",(req,res)=>{    
     res.send("hello from backend")  
 })   
 
