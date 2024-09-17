51ddcc0dbdca8bcadfc7f86caed7698b8c5b541e
mgore <manojgore.mg@gmail.com>
2024-09-17 09:03:19 +0530

    h

--- frontend/src/components/BrowseByType.js
+++ frontend/src/components/BrowseByType.js
@@ -109,7 +109,7 @@ function BrowseByType() {
                 <div class="image-box">
                   <svg
                     width="34"
-                    height="34"
+                    height="34" 
                     viewBox="0 0 34 34"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
