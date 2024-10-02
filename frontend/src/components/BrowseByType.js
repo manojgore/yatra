3d94e90c43dc49886f2f86dbe6e1723354c0b6d0
mgore <manojgore.mg@gmail.com>
2024-10-02 10:45:35 +0530

    h

--- frontend/src/components/BrowseByType.js
+++ frontend/src/components/BrowseByType.js
@@ -270,7 +270,7 @@ function BrowseByType() {
                         stroke="#050B20"
                         stroke-width="2"
                         stroke-miterlimit="10"
-                        stroke-linecap="round"
+                        stroke-linecap="round" 
                         stroke-linejoin="round"
                       />
                       <path
