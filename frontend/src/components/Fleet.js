aa3ec2ab3352c6850ae62ce0409fba6f0c3a40e3
mgore <manojgore.mg@gmail.com>
2024-10-06 09:26:48 +0530

    g

--- frontend/src/components/Fleet.js
+++ frontend/src/components/Fleet.js
@@ -47,7 +47,7 @@ function Fleet() {
                                     <li><a href="#">Used Ford Cars</a></li>
                                     <li><a href="#">Used Honda Cars</a></li>
                                     <li><a href="#">Used Hyundai Cars</a></li>
-                                    <li><a href="#">Used Infiniti Cars</a></li>
+                                    <li><a href="#">Used Infiniti Cars</a></li> 
                                     <li><a href="#">Used Jaguar Cars</a></li>
                                     <li><a href="#">Used Jeep Cars</a></li>
                                 </ul>
